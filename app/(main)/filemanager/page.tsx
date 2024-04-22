91ce4366b8b57bd1b373c494919cc4ab093217e5
leductoanuit <leductoan1598@gmail.com>
2024-04-22 14:55:24 +0700

    update

--- app/(main)/filemanager/page.tsx
+++ app/(main)/filemanager/page.tsx
@@ -5,7 +5,7 @@ const FileManager = () => {
     return (
         <div>
             <iframe
-                src="https://sogecgas.com:8080/"
+                src="https://filemanager.sogecgas.com/"
                 width="100%"
                 height="800px"
                 style={{ border: "none" }}
