03da48c9315a159993793f9b503923dc5879ace4
trangnguyenthanh <104630137+trangnguyenthanh@users.noreply.github.com>
2024-08-05 10:32:25 +0700

    update

--- app/(main)/SetupData/LowHighData/LowHighData.tsx
+++ app/(main)/SetupData/LowHighData/LowHighData.tsx
@@ -3218,7 +3218,7 @@ const handleCheckboxChangePLC = (e:any) => {
                     DI_MAP_1_High:inputValueDI_MAP_1, DI_MAP_1_Low:inputValue2DI_MAP_1,
                    DI_ZSC_1_High:inputValueDI_ZSC_1, DI_ZSC_1_Low:inputValue2DI_ZSC_1,
                    DI_ZSO_1_High:inputValueDI_ZSO_1, DI_ZSO_1_Low:inputValue2DI_ZSO_1,
-                    DO_SV_01_High:inputValueDO_SV1, DO_SV_01_Low:inputValue2DO_SV1,
+                    DO_SV1_High:inputValueDO_SV1, DO_SV1_Low:inputValue2DO_SV1,
                     DO_BC_01_High:inputValueDO_BC_01, DO_BC_01_Low:inputValue2DO_BC_01,
 
 
